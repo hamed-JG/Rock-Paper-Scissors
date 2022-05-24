5726763b5822118449e955ace1d4ef2a1fb4f0e2
Hamed Jahan <iamhamedjahan@gmail.com>
2022-05-24 19:23:25 +0430

    HTML and main.js are added

--- main.js
+++ main.js
@@ -1,64 +1,34 @@
-const usernameInput = document.querySelector(".user-input");
-const passwordInput = document.querySelector(".pass-input");
-const usernameMsg = document.querySelector(".username-msg");
-const passwordMsg = document.querySelector(".password-msg");
-const sigininMsg = document.querySelector(".signin-status");
-const siginBtn = document.querySelector(".signin-button");
+const choices = ["rock", "paper", "scissors"];
 
-siginBtn.addEventListener("click", signIn);
-
-function signIn(event) {
-  event.preventDefault();
-  usernameMsg.innerText = "";
-  passwordMsg.innerText = "";
-  const usernameValue = usernameInput.value;
-  const passwordValue = passwordInput.value;
-  let ifSendData = true;
-
-  if (
-    usernameValue.length === 0 ||
-    usernameValue.indexOf("@") === -1 ||
-    usernameValue.indexOf(".") === -1
-  ) {
-    usernameMsg.innerText = "Please enter a valid Email";
-    ifSendData = false;
-  }
-
-  if (passwordValue.length === 0) {
-    passwordMsg.innerText = "Please enter your password";
-    ifSendData = false;
-  } else if (passwordValue.length <= 4) {
-    passwordMsg.innerText = "Your password is too short";
-    ifSendData = false;
-  }
-
-  if (ifSendData) {
-    const body = JSON.stringify({
-      username: usernameValue,
-      password: passwordValue,
-    });
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: body,
-      headers: headers,
-    }).then((response) => {
-      if (response.ok) {
-        sigininMsg.innerText = "You signed in successfully";
-      }
-    });
-  }
+const userChoice = prompt("Choose rock, paper, scissors");
+if (userChoice) {
+    console.log(`You choose: ${userChoice}`);
+} else {
+    console.log("You cheated!")
 }
-fetch("http://localhost:3300/auth/login", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    email: "iamhamedjahan@gmail.com",
-    password: "Hj701026",
-  }),
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
 
+const randomNumber = Math.floor(Math.random() * 3);
+const computerChoice = choices[randomNumber];
+console.log(`Computer chooses: ${computerChoice}`);
+
+if (userChoice === computerChoice) {
+    console.log("It's a tie");
+} else if (userChoice === "rock") {
+    if (computerChoice === "scissors") {
+        console.log("You Win!");
+    } else {
+        console.log("you lose!")
+    }
+} else if (userChoice === "paper") {
+    if (computerChoice === "rock") {
+        console.log("You win!")
+    } else {
+        console.log("You lose!")
+    }
+} else if (userChoice === "scissors") {
+    if (computerChoice === "paper") {
+        console.log("You win!")
+    } else {
+        console.log("You lose!")
+    }
+}
